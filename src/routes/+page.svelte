bdb427e01a4185789ca800ca66d1c18a05dbc261
Thomas <dd_tommie@hotmail.com>
2023-12-26 19:44:35 +0100

    added tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This page is to welcome you</p>
+<table>
+    <tr>
+        <td>Cell 1</td>
+        <td>Cell 2</td>
+        <td>Cell 3</td>
+    </tr>
+    <tr>
+        <td>Cell 4</td>
+        <td>Cell 5</td>
+        <td>Cell 6</td>
+    </tr>
+    <tr>
+        <td>Cell 7</td>
+        <td>Cell 8</td>
+        <td>Cell 9</td>
+    </tr>
+</table>
