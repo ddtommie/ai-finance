0a8c32cf32b2acde6d52a5d866208699a7949af5
Thomas <dd_tommie@hotmail.com>
2024-01-05 10:40:09 +0100

    changed the order of analytics ?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
-    
-    inject({ mode: dev ? 'development' : 'production' });
-    
     import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
-    injectSpeedInsights();
     
+    injectSpeedInsights();
+
+    inject({ mode: dev ? 'development' : 'production' });
+
     import './styles.css';
 </script>
 
